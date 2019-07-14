4367ec3951b089bd5e2822e8321744759717ac70
Bogdan Popa <bogdan@defn.io>
2019-07-14 15:24:53 +0300

    query: test query props

--- deta-test/deta/query.rkt
+++ deta-test/deta/query.rkt
@@ -2,6 +2,7 @@
 
 (require db
          deta
+         racket/port
          rackunit)
 
 (provide
@@ -18,7 +19,26 @@
     "prop:statement"
 
     (test-case "queries can be executed by the standard query functions"
-      (check-equal? (query-value conn (select 1)) 1)))))
+      (check-equal? (query-value conn (select 1)) 1))
+
+    (test-case "queries with placeholders can be executed by the standard query functions"
+      (check-equal? (query-value conn (select ,1)) 1)))
+
+   (test-suite
+    "prop:custom-write"
+
+    (test-case "prints queries"
+      (check-equal?
+       (with-output-to-string
+        (lambda _
+          (display (select 1))))
+       "#<query: SELECT 1>")
+
+      (check-equal?
+       (with-output-to-string
+        (lambda _
+          (display (select ,1))))
+       "#<query: SELECT $1 1>")))))
 
 (module+ test
   (require rackunit/text-ui)
