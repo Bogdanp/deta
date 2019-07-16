86ee330a2a0fa8040321ecfc534869bd3007fcfc
Bogdan Popa <bogdan@defn.io>
2019-07-16 21:08:55 +0300

    ci: build and upload docs in actions

--- .github/main.workflow
+++ .github/main.workflow
@@ -18,4 +18,9 @@ action "docs" {
   needs = ["build"]
   uses = "docker://bogdanp/racket:7.3"
   runs = "/github/workspace/ci/docs.sh"
+  secrets = [
+    "DETA_DOCS_DEPLOY_KEY_PASSPHRASE",
+    "DETA_DOCS_SSH_PORT",
+    "DETA_DOCS_SSH_HOST"
+  ]
 }
--- ci/docs.sh
+++ ci/docs.sh
@@ -2,6 +2,17 @@
 
 set -euo pipefail
 
+apt-get update
+apt-get install -y --no-install-recommends gettext-base gpg ssh rsync
+
 pushd /github/workspace
 scribble +m --dest doc --redirect-main 'http://docs.racket-lang.org/' deta-doc/deta.scrbl
+mv doc/deta.html doc/index.html
+
+mkdir -p /tmp/secrets
+gpg -q --batch --yes --decrypt --passphrase="$DETA_DOCS_DEPLOY_KEY_PASSPHRASE" -o /tmp/secrets/deploy ci/deploy.gpg
+chmod 0600 /tmp/secrets/deploy
+rsync \
+    -e "ssh -P $DETA_DOCS_SSH_PORT -o StrictHostKeyChecking=no -i /tmp/secrets/deploy" \
+    -a doc/ deta@"$DETA_DOCS_SSH_HOST":~/www/
 popd
