3fcd3f9a69373aa063e8db5e0b21c9f60c3b894c
Bogdan Popa <bogdan@defn.io>
2019-09-28 13:02:57 +0300

    ci: install sqlite3 3.29 during build

--- ci/build.sh
+++ ci/build.sh
@@ -3,5 +3,8 @@
 set -euo pipefail
 
 pushd /github/workspace
+# TODO: remove once libsqlite3-x86_64-linux is on the pkg server
+raco pkg install --name libsqlite3-x86_64-linux https://racket.defn.io/libsqlite3-x86_64-linux-3.29.0.tar.gz
 raco pkg install --auto --batch deta-lib/ deta-doc/ deta-test/
+raco test -j 4 deta-test/
 popd
