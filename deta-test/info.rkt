d3bf2a2c54c74c23e0b1a19f433bd53eadc4471d
Bogdan Popa <bogdan@defn.io>
2019-09-28 19:55:34 +0300

    test: fixup platform declaration

--- deta-test/info.rkt
+++ deta-test/info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "tests")
 
-(define deps '(("libsqlite3-x86_64-linux" #:platform "x86_64-linux")))
+(define deps '(("libsqlite3-x86_64-linux" #:platform #rx"x86_64-linux")))
 (define build-deps '("base"
                      "at-exp-lib"
                      "db-lib"
