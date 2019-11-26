574a514dadd3088d5521efefa930bad7d050f788
Bogdan Popa <bogdan@defn.io>
2019-11-26 11:47:16 +0200

    ci: use setup-racket

--- ci/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-pushd /github/workspace
-raco pkg install --auto --batch deta-lib/ deta-doc/ deta-test/
-raco test -j 4 deta-test/
-popd
--- ci/docs.sh
+++ ci/docs.sh
@@ -2,10 +2,9 @@
 
 set -euo pipefail
 
-apt-get update
-apt-get install -y --no-install-recommends gettext-base gpg ssh rsync
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends gettext-base gpg ssh rsync
 
-pushd /github/workspace
 scribble +m --dest doc --redirect 'http://docs.racket-lang.org/' deta-doc/deta.scrbl
 mv doc/deta.html doc/index.html
 
@@ -15,4 +14,3 @@ chmod 0600 /tmp/secrets/deploy
 rsync \
     -e "ssh -p $DETA_DOCS_SSH_PORT -o StrictHostKeyChecking=no -i /tmp/secrets/deploy" \
     -a doc/ deta@"$DETA_DOCS_SSH_HOST":~/www/
-popd
