318b6a279ddf0aeca4f1af787a46f3c648d37f78
Bogdan Popa <bogdan@defn.io>
2022-08-12 08:24:25 +0300

    build: avoid bringing own sqlite3 on natipkg
    This is an attempt to fix the build on pkg.racket-lang.org.

--- deta-test/info.rkt
+++ deta-test/info.rkt
@@ -2,13 +2,15 @@
 
 (define collection "tests")
 
-(define deps '(("libsqlite3-x86_64-linux" #:platform #rx"x86_64-linux")))
-(define build-deps '("base"
-                     "at-exp-lib"
-                     "db-lib"
-                     "deta-lib"
-                     "gregor-lib"
-                     "rackunit-lib"
-                     "threading-lib"))
+(define deps '())
+(define build-deps
+  '("base"
+    "at-exp-lib"
+    "db-lib"
+    "deta-lib"
+    "gregor-lib"
+    ["libsqlite3-x86_64-linux" #:platform #rx"x86_64-linux(?!-natipkg)"]
+    "rackunit-lib"
+    "threading-lib"))
 
 (define update-implies '("deta-lib"))
