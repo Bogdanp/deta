409784fda2036f07aab69ad3625a08dbe4465984
Bogdan Popa <bogdan@defn.io>
2024-02-11 15:19:11 +0200

    lint: add linter for define-type

--- deta-lint-lib/review.rkt
+++ deta-lint-lib/review.rkt
@@ -10,8 +10,9 @@
  review-syntax)
 
 (define (should-review-syntax? stx)
-  (syntax-case stx (define-schema)
+  (syntax-case stx (define-schema define-type)
     [(define-schema . _rest) #t]
+    [(define-type . _rest) #t]
     [_ #f]))
 
 (define-splicing-syntax-class schema-field-option
@@ -56,7 +57,26 @@
                       #:check-usages? #f
                       (format "~a~a-~~a" p schema-id-sym))))]))
 
+(define-syntax-class type-definition
+  #:datum-literals (define-type)
+  (pattern (define-type
+             ~!
+             type-id:id
+             {~do (push-scope)}
+             {~optional (field-id:id ...)}
+             {~alt {~optional {~seq {~or #:contract
+                                         #:contract-fn}
+                                    contract-expr:expression}}
+                   {~optional {~seq #:declaration declaration-expr:expression}}
+                   {~optional {~seq #:constructor constructor-expr:expression}}
+                   {~optional {~seq #:dump dump-expr:expression}}
+                   {~optional {~seq #:load load-expr:expression}}} ...)
+           #:do [(pop-scope)
+                 (track-binding #'type-id "~a/f" #:check-usages? #f)
+                 (track-binding #'type-id "~a/f?" #:check-usages? #f)]))
+
 (define (review-syntax stx)
   (syntax-parse stx
     [d:schema-definition #'d]
-    [_ (track-error stx "expected a deta schema definition")]))
+    [t:type-definition #'t]
+    [_ (track-error stx "expected a deta schema or type definition")]))
